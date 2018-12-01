7d895b9600e007bf87d5c828a4ae964db7e190e2
Jeff Peng <jeffpeng@Jeffs-MacBook-Pro.local>
2018-12-01 16:05:45 -0500

    make callgbcut endpoint call gbcut.py

--- gbcut.py
+++ gbcut.py
@@ -3,29 +3,39 @@ import cv2 as cv
 import matplotlib
 matplotlib.use("MacOSX")
 import matplotlib.pyplot as plt
-oriimg = cv.imread('cracker.jpg')
-height, width, channels = oriimg.shape
-W = 1000
-imgScale = W/width
-newX,newY = oriimg.shape[1]*imgScale, oriimg.shape[0]*imgScale
-newimg = cv.resize(oriimg,(int(newX),int(newY)))
 
-mask = np.zeros(newimg.shape[:2],np.uint8)
-bgdModel = np.zeros((1,65),np.float64)
-fgdModel = np.zeros((1,65),np.float64)
+def gbcutImg(picname):
+    #inputname = "banana.jpg" example
+    #outputname = "banana.png" example
+    inputname = picname + 'jpg'
+    outputname = picname + 'png'
+    #rescale img
+    oriimg = cv.imread(inputname)
+    height, width, channels = oriimg.shape
+    W = 480
+    imgScale = W/width
+    newX,newY = oriimg.shape[1]*imgScale, oriimg.shape[0]*imgScale
+    newimg = cv.resize(oriimg,(int(newX),int(newY)))
 
-heightnew, widthnew, channelsnew = newimg.shape
-print(heightnew)
-print(widthnew)
+    #create mask for newimg
+    mask = np.zeros(newimg.shape[:2],np.uint8)
+    bgdModel = np.zeros((1,65),np.float64)
+    fgdModel = np.zeros((1,65),np.float64)
+    heightnew, widthnew, channelsnew = newimg.shape
+    
+    print(heightnew) #debug
+    print(widthnew)
 
-rect = (1,1,widthnew-1,heightnew-1)
-cv.grabCut(newimg,mask,rect,bgdModel,fgdModel,15,cv.GC_INIT_WITH_RECT)
-mask2 = np.where((mask==1) + (mask==3),255,0).astype('uint8')
-#newimg = newimg*mask2[:,:,np.newaxis]
-newimg = cv.bitwise_and(newimg,newimg,mask=mask2)
-'''
-bar = np.zeros((newimg.shape[0],5,3),np.uint8)
-res = np.hstack((new,bar,img,bar,output))
-'''
-cv.imwrite('output.png',newimg)
-plt.imshow(newimg),plt.colorbar(),plt.show()
+    #rect assuming the user has already pre cropped the image
+    rect = (1,1,widthnew-1,heightnew-1)
+
+    #running grabCut 15 times for good luck :)
+    cv.grabCut(newimg,mask,rect,bgdModel,fgdModel,15,cv.GC_INIT_WITH_RECT)
+    mask2 = np.where((mask==1) + (mask==3),255,0).astype('uint8')
+    #newimg = newimg*mask2[:,:,np.newaxis]
+    newimg = cv.bitwise_and(newimg,newimg,mask=mask2)
+
+    #save the segmented image to local
+    cv.imwrite(outputname,newimg)
+    print('finish segment')
+    #plt.imshow(newimg),plt.colorbar(),plt.show()
--- uploadTest.py
+++ uploadTest.py
@@ -1,6 +1,14 @@
 from flask import Flask, render_template, request
 from werkzeug import secure_filename
+
+import numpy as np
+import cv2 as cv
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+
 app = Flask(__name__)
+from gbcut import gbcutImg
 
 @app.route('/upload')
 def upload_file():
@@ -19,6 +27,21 @@ def uploadr_file():
       #the name here will have to be changed to something more meaningful
       #f.save(secure_filename(f.filename))
       f.save(name)
+      return 'file uploaded successfully'
+
+@app.route('/')
+def test_file():
+   return render_template('uploadImg.html')
+
+@app.route('/callgbcut', methods = ['GET', 'POST'])
+def gbcutcall():
+   if request.method == 'POST':
+      f = request.files['file']
+      name = request.form['itemname']
+      #the name here will have to be changed to something more meaningful
+      #f.save(secure_filename(f.filename))
+      f.save(name)
+      gbcutImg(name)
       return 'file uploaded successfully'
 		
 if __name__ == '__main__':
