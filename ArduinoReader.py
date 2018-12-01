ab1e6fd28aa9424fa48876dfd61d1bcf651e25c2
phophores <paulyf.wang@mail.utoronto.ca>
2018-12-01 10:15:22 -0500

    logo

--- ArduinoReader.py
+++ ArduinoReader.py
@@ -6,44 +6,42 @@ import requests
 import urllib
 from flask import Flask
 
-
 app = Flask(__name__)
-ser = serial.Serial('COM8', 9600, timeout=1000)
+ser = serial.Serial('COM8', 9600, timeout=1)
 time.sleep(3)
 
 def arduinoToFloat(ser):
     return float(next(iter(re.findall(r'-?\d+\.?\d*',ser.readline().decode("utf-8"))), 0))
 
+@app.route('/getWeight', methods=['GET'])
+def restGet():
+    return getCurrentWeight(ser)
+
 def getCurrentWeight(ser):
     while(not ser.in_waiting):
         continue
-    return arduinoToFloat(ser)
+    return str(arduinoToFloat(ser))
+
 def sendCurrentWeight(ser):
     if (ser.in_waiting):
         weight = str(getCurrentWeight(ser))
-        requests.post('https://wuwicajon.wixsite.com/test/_functions-dev/setWeight',
-        params=urllib.parse.urlencode({
-        'weight': weight,}))
-
+        print(weight)
+        resp = requests.post('https://wuwicajon.wixsite.com/test/_functions-dev/setWeight?weight=%s' % weight)
+        print(resp)
+        
 @app.route('/tare', methods=['GET'])
 def tareThenGetWeight():
     ser.flush()
     while (not ser.writable):
         continue
     ser.write(b't')
-
     return 'ok'
+
 if __name__ == '__main__':
    app.run(host='localhost')
-print("ASD")
-while True:
-    sendCurrentWeight(ser)
-   # if (not ser.in_waiting):
-     #   weight = str(getCurrentWeight(ser))
-     #   resp=requests.post('https://wuwicajon.wixsite.com/test/_functions-dev/setWeight',
-     #   params=urllib.parse.urlencode({
-     #   'weight': weight,}))
-     #   print (resp)
+else:
+    while True:
+        sendCurrentWeight(ser)
 
 
 
